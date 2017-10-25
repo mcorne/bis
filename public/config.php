4125c418c6477daa33ae985a7cf64f4278673a85
Michel Corne <mcorne@yahoo.com>
2017-10-25 16:55:07 +0200

    added fontawesome and w3css

---
+++ public/config.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    'development' => array(
+        'base_path'  => __DIR__ . '/../application',
+        'base_url'   => sprintf('http://%s/bis', $_SERVER['HTTP_HOST']), // the domain subpath MUST BE the same as the one used in .htaccess
+        'data_path'  => __DIR__ . '/../application/data.development',
+    ),
+    'production'  => array(
+        'base_path'  => __DIR__ . '/../../cgi-bin/bis',
+        'base_url'   => sprintf('http://%s/bis', $_SERVER['HTTP_HOST']), // the domain subpath MUST BE the same as the one used in .htaccess
+        'data_path'  => __DIR__ . '/../../cgi-bin/bis/data.production',
+    ),
+);
