772f1fd74c818cebb5d0c96068a7bfd52c192445
Michel Corne <mcorne@yahoo.com>
2017-11-24 12:01:43 +0100

    fixed the application name

--- public/config.php
+++ public/config.php
@@ -2,12 +2,12 @@
 return array(
     'development' => array(
         'base_path'  => __DIR__ . '/../application',
-        'base_url'   => sprintf('http://%s/bis', $_SERVER['HTTP_HOST']), // the domain subpath MUST BE the same as the one used in .htaccess
+        'base_url'   => sprintf('http://%s/bits', $_SERVER['HTTP_HOST']), // the domain subpath MUST BE the same as the one used in .htaccess
         'data_path'  => __DIR__ . '/../application/data/development.issues.sqlite',
     ),
     'production'  => array(
-        'base_path'  => __DIR__ . '/../../cgi-bin/bis',
-        'base_url'   => sprintf('http://%s/bis', $_SERVER['HTTP_HOST']), // the domain subpath MUST BE the same as the one used in .htaccess
-        'data_path'  => __DIR__ . '/../../cgi-bin/bis/data/issues.sqlite',
+        'base_path'  => __DIR__ . '/../../cgi-bin/bits',
+        'base_url'   => sprintf('http://%s/bits', $_SERVER['HTTP_HOST']), // the domain subpath MUST BE the same as the one used in .htaccess
+        'data_path'  => __DIR__ . '/../../cgi-bin/bits/data/issues.sqlite',
     ),
 );
